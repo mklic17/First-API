4638e1d980229481502e4fcb5719573596f452b0
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-08 12:06:48 -0400

    A new joke appears upon every page refresh

--- getJoke.js
+++ getJoke.js
@@ -1,1 +1,19 @@
 var url = 'http://api.icndb.com/jokes/random';
+//function When are
+// have some event handler
+
+function addJokes(joke){
+  $('.jokeGoesHere').append('<li>' + joke + '</li>');
+}
+
+$.get({
+  //method : 'get'
+  url: url,
+  success: function(results){
+    //process the data
+    console.log(results);
+    console.log(results.value.joke);
+    addJokes(results.value.joke);
+
+  },
+})
