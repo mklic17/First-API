dec868fd11e2641cea5221c8352762066894301b
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-09 01:35:16 -0400

    The 'get' request is working and is pulling all of the current names in the dataBase.

---
+++ Xtern/Rivalry/app.js
@@ -0,0 +1,14 @@
+var studentURL = 'https://xtern-roster.herokuapp.com/people';
+var rivalryURL = 'https://xtern-roster.herokuapp.com/rivalries'; // you will need a query string to continue
+
+function AddTheStudentsToList(studentNames){
+  for (var i = 0; i < studentNames.length; i++){
+    var name = studentNames[i].name
+    $('#StudentNames').append('<li>' + name + '</li>');
+}
+}
+
+$.get({
+  url: studentURL,
+  success: AddTheStudentsToList,
+})
