2e38ab3980c9676c4b9580193142ac5831b4e396
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-09 00:57:49 -0400

    Multiple requests will process when the button is clicked.

--- getJoke.js
+++ getJoke.js
@@ -1,19 +1,20 @@
+//$(document).on('click', 'a.Another Joke', handleThis);
+
 var url = 'http://api.icndb.com/jokes/random';
-//function When are
-// have some event handler
+
+function doSomething() {
+  console.log('Yaaaa');
+  // ev.preventDefault();
+  // ev.currentTarget;
+  $.get({
+    url: url,
+    success: addJokes
+
+  })
+};
+
 
 function addJokes(joke){
-  $('.jokeGoesHere').append('<li>' + joke + '</li>');
+  var things = joke.value.joke;
+  $('.jokeGoesHere').append('<li>' + things + '</li>');
 }
-
-$.get({
-  //method : 'get'
-  url: url,
-  success: function(results){
-    //process the data
-    console.log(results);
-    console.log(results.value.joke);
-    addJokes(results.value.joke);
-
-  },
-})
