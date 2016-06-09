e0548689b4e6868d092cece8e556a7576d792a71
Mitchell Klich <MitchellKlich_2017@DePauw.edu>
2016-06-09 01:45:07 -0400

    the code is more modular for pulling from an API.

--- Xtern/Rivalry/app.js
+++ Xtern/Rivalry/app.js
@@ -2,10 +2,14 @@ var studentURL = 'https://xtern-roster.herokuapp.com/people';
 var rivalryURL = 'https://xtern-roster.herokuapp.com/rivalries'; // you will need a query string to continue
 
 function AddTheStudentsToList(studentNames){
-  for (var i = 0; i < studentNames.length; i++){
-    var name = studentNames[i].name
-    $('#StudentNames').append('<li>' + name + '</li>');
-}
+  // for (var i = 0; i < studentNames.length; i++){
+
+    $.each(studentNames, function(i, name){
+      var name = studentNames[i].name
+      $('#StudentNames').append('<li>' + name + '</li>');
+    })
+
+
 }
 
 $.get({
